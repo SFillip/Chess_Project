cf3f2bd31feaf7b7c845e36e508dcf1e3d45beea
SFillip <Itsab338@gmail.com>
2022-01-13 23:16:37 +0100

    Commit of server and client connection events

--- code/src/main/java/com/chessproject/networking/events/ConnectionListener.java
+++ code/src/main/java/com/chessproject/networking/events/ConnectionListener.java
@@ -1,5 +1,6 @@
 package com.chessproject.networking.events;
 
+/**Event that riggers when a client connects to the Server*/
 @FunctionalInterface
 public interface ConnectionListener {
     void onConnect();
--- code/src/main/java/com/chessproject/networking/events/HostListener.java
+++ code/src/main/java/com/chessproject/networking/events/HostListener.java
@@ -1,5 +1,6 @@
 package com.chessproject.networking.events;
 
+/**Event that triggers when the Host starts up the connection*/
 @FunctionalInterface
 public interface HostListener {
     void onHostStarted();
