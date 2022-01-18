d9c92f453550f10b0a403078666a608f2ac45b1d
phartl19 <Hartl.Paul@sus.htl-grieskirchen.at>
2022-01-18 12:43:15 +0100

    merge setup commit

---
+++ code/src/main/java/com/chessproject/JoinContainer.java
@@ -0,0 +1,18 @@
+package com.chessproject;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class JoinContainer {
+    @javafx.fxml.FXML
+    private TextField ipTextfield;
+    @javafx.fxml.FXML
+    private Button backToMenue;
+    @javafx.fxml.FXML
+    private TextField playerName;
+    @javafx.fxml.FXML
+    private Label hiddenLabel;
+    @javafx.fxml.FXML
+    private Button joinTheGame;
+}
