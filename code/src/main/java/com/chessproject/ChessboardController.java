e0719b8b7cd071343b4a140dbfd0d165ffdc96b4
SFillip <Itsab338@gmail.com>
2022-02-01 13:57:02 +0100

    On Hover Event Commit

--- code/src/main/java/com/chessproject/ChessboardController.java
+++ code/src/main/java/com/chessproject/ChessboardController.java
@@ -1,4 +1,30 @@
 package com.chessproject;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
 public class ChessboardController {
+
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    public void initialize(){
+        for(Node child : gridPane.getChildren()){
+
+            child.setOnMouseEntered(new EventHandler<MouseEvent>(){
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    Pane p=(Pane) mouseEvent.getSource();
+
+                }
+            });
+        }
+    }
 }
